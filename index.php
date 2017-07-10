83bf2831923b5120db422bfc6fae5dd86c8d15a3
Fritz <fre.healy@gmail.com>
2017-07-10 15:06:29 -0400

    adding index page pull

--- index.php
+++ index.php
@@ -16,7 +16,8 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			<?php $post = get_post(40);
+			setup_postdata($post);?>
 			<?php get_template_part( 'template-parts/content', "index" );?>
 
 		</main><!-- #main -->
