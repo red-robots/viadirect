4c282c4c9464aef8ebb8c22f8b1dbb8d2ecca6cb
Fritz <fre.healy@gmail.com>
2017-08-01 14:24:24 -0400

    styling viatrack

--- template-parts/content-via-track.php
+++ template-parts/content-via-track.php
@@ -11,10 +11,9 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class("template-via-track"); ?>>
 	<?php $header_background_image = get_field("header_background_image");
-	$row_1_sub_heading = get_field("row_1_sub_heading");
-	$row_2_heading = get_field("row_2_heading");
+	$row_1_copy = get_field("row_1_copy");
+	$row_1_image = get_field("row_1_image");
 	$row_2_copy = get_field("row_2_copy");
-	$row_3_heading = get_field("row_3_heading");
 	$row_4_heading = get_field("row_4_heading");
 	$row_6_images = get_field("row_6_images");
 	$request_demo_text = get_field("request_demo_text");
@@ -27,74 +26,28 @@
 	<div class="row-1">
 		<header>
 			<h1><?php the_title();?></h1>
-			<?php if($row_1_sub_heading):?>
-				<div class="sub-heading">
-					<?php echo $row_1_sub_heading;?>
-				</div><!--.sub-heading-->
-			<?php endif;?>
 		</header>
-	</div><!--.row-1-->
-	<section class="row-2 clear-bottom">
-		<?php if($row_2_heading):?>
-			<header>
-				<h2><?php echo $row_2_heading;?></h2>
-			</header>
+		<div class="wrapper clear-bottom">
+			<div class="col-1 copy">
+				<?php if($row_1_copy) echo $row_1_copy;?>
+			</div><!--.col-1-->
+			<div class="col-2">
+				<?php if($row_1_image):?> 
+					<img src="<?php echo $row_1_image['sizes']['large'];?>" alt="<?php echo $row_1_image['alt'];?>">
+				<?php endif;?>
+			</div><!--.col-2-->
+		</div><!--.wrapper-->
+		<?php if($request_demo_link && $request_demo_text):?>
+			<div class="button">
+				<a href="<?php echo $request_demo_link;?>">
+					<?php echo $request_demo_text;?>
+				</a>
+			</div><!--.button-->
 		<?php endif;?>
-		<div class="col-1 svg">
-			<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 132.14 134.39">
-				<defs>
-					<style>
-					.cls-1 {
-						fill: #fff;
-					}
-					</style>
-				</defs>
-				<title>Untitled-14</title>
-				<path class="cls-1" d="M171,106V77.67q0-27.13-19.47-46.6T104.95,11.6q-27.14,0-46.6,19.47T38.88,77.67V146H67.19V77.67A36.38,36.38,0,0,1,78.25,51a36.37,36.37,0,0,1,26.69-11.06A36.38,36.38,0,0,1,131.64,51,36.38,36.38,0,0,1,142.7,77.67V106Z" transform="translate(-38.88 -11.6)"/>
-			</svg>
-			<svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 169.9 113.25">
-				<defs>
-					<style>
-					.cls-1 {
-						fill: #fff;
-					}
-					</style>
-				</defs>
-				<title>lockbottom</title>
-				<path class="cls-1" d="M23.13,147.11a13.65,13.65,0,0,0-4.13,10v84.95a14.1,14.1,0,0,0,14.16,14.16H174.74a14.1,14.1,0,0,0,14.16-14.16V157.14A14.08,14.08,0,0,0,175.11,143H32.79A13.62,13.62,0,0,0,23.13,147.11Z" transform="translate(-19 -143)"/>
-			</svg>
-		</div><!--.col-1-->
-		<div class="col-2">
-			<?php if($row_2_copy):?>
-				<?php foreach($row_2_copy as $row):?>
-					<div class="row">
-						<div class="col-1">
-							<?php if($row['icon']) echo $row['icon'];?>
-						</div><!--.col-1-->
-						<div class="col-2 copy">
-							<?php if($row['copy']) echo $row['copy'];?>
-						</div><!--.col-2-->
-					</div><!--.row-->
-				<?php endforeach;?>
-			<?php endif;?>
-		</div><!--.col-2-->
+	</div><!--.row-1-->
+	<section class="row-2 copy">
+		<?php echo $row_2_copy;?>
 	</section><!--.row-2-->
-	<?php if($row_3_heading || ($request_demo_link && $request_demo_text)):?>
-		<div class="row-3">
-			<?php if($row_3_heading):?>
-				<div class="heading">
-					<?php echo $row_3_heading;?>
-				</div>
-			<?php endif;?>
-			<?php if($request_demo_link && $request_demo_text):?>
-				<div class="button">
-					<a href="<?php echo $request_demo_link;?>">
-						<?php echo $request_demo_text;?>
-					</a>
-				</div><!--.button-->
-			<?php endif;?>
-		</div><!--.row-3-->
-	<?php endif;?>
 	<?php if($row_4_heading):?>
 		<div class="row-4">
 			<header>
