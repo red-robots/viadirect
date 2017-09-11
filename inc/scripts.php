4d4877c42fdc6cea604255a417fe08d1c5e451c4
Fritz <fre.healy@gmail.com>
2017-09-11 14:27:46 -0400

    changing to ssl

--- inc/scripts.php
+++ inc/scripts.php
@@ -13,14 +13,14 @@ function acstarter_scripts() {
 
 	wp_enqueue_script( 
 			'acstarter-blocks', 
-			get_template_directory_uri() . '/assets/js/vendors.js', 
+			get_template_directory_uri() . '/assets/js/vendors.min.js', 
 			array('jquery'), '20120206', 
 			true 
 		);
 
 	wp_enqueue_script( 
 			'acstarter-custom', 
-			get_template_directory_uri() . '/assets/js/custom.js', 
+			get_template_directory_uri() . '/assets/js/custom.min.js', 
 			array('jquery','acstarter-blocks'), '20120206', 
 			true 
 		);
