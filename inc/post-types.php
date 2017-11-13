f33795b328b8d25ff44e8d3edb4a7c9b0f443bad
Fritz <fre.healy@gmail.com>
2017-11-13 10:24:22 -0500

    adding events post type

--- inc/post-types.php
+++ inc/post-types.php
@@ -38,19 +38,50 @@ function js_custom_init()
     ); 
     register_post_type('testimonial',$args); // name used in query
 
+  $labels = array(
+    'name' => _x('Team', 'post type general name'),
+      'singular_name' => _x('Team', 'post type singular name'),
+      'add_new' => _x('Add New', 'Team Member'),
+      'add_new_item' => __('Add New Team Member'),
+      'edit_item' => __('Edit Team Member'),
+      'new_item' => __('New Team Member'),
+      'view_item' => __('View Team Member'),
+      'search_items' => __('Search Team Members'),
+      'not_found' =>  __('No Team Members found'),
+      'not_found_in_trash' => __('No Team Members found in Trash'), 
+      'parent_item_colon' => '',
+      'menu_name' => 'Team'
+    );
+    $args = array(
+    'labels' => $labels,
+      'public' => true,
+      'publicly_queryable' => true,
+      'show_ui' => true, 
+      'show_in_menu' => true, 
+      'query_var' => true,
+      'rewrite' => true,
+      'capability_type' => 'post',
+      'has_archive' => false, 
+      'hierarchical' => false, // 'false' acts like posts 'true' acts like pages
+      'menu_position' => 20,
+      'supports' => array('title','editor','custom-fields','thumbnail'),
+    
+    ); 
+    register_post_type('team',$args); // name used in query
+
     $labels = array(
-      'name' => _x('Team', 'post type general name'),
-        'singular_name' => _x('Team', 'post type singular name'),
-        'add_new' => _x('Add New', 'Team Member'),
-        'add_new_item' => __('Add New Team Member'),
-        'edit_item' => __('Edit Team Member'),
-        'new_item' => __('New Team Member'),
-        'view_item' => __('View Team Member'),
-        'search_items' => __('Search Team Members'),
-        'not_found' =>  __('No Team Members found'),
-        'not_found_in_trash' => __('No Team Members found in Trash'), 
+      'name' => _x('Events', 'post type general name'),
+        'singular_name' => _x('Event', 'post type singular name'),
+        'add_new' => _x('Add New', 'Event'),
+        'add_new_item' => __('Add New Event'),
+        'edit_item' => __('Edit Events'),
+        'new_item' => __('New Event'),
+        'view_item' => __('View Events'),
+        'search_items' => __('Search Events'),
+        'not_found' =>  __('No Events found'),
+        'not_found_in_trash' => __('No Events found in Trash'), 
         'parent_item_colon' => '',
-        'menu_name' => 'Team'
+        'menu_name' => 'Events'
       );
       $args = array(
       'labels' => $labels,
@@ -67,7 +98,7 @@ function js_custom_init()
         'supports' => array('title','editor','custom-fields','thumbnail'),
       
       ); 
-      register_post_type('team',$args); // name used in query
+      register_post_type('event',$args); // name used in query
   
   // Add more between here
   
