600cf0d730f5f8d54ff54be2804b1d662a7da01b
Fritz <fre.healy@gmail.com>
2017-11-13 12:21:01 -0500

    changing stylesheet pull

--- inc/scripts.php
+++ inc/scripts.php
@@ -3,7 +3,7 @@
  * Enqueue scripts and styles.
  */
 function acstarter_scripts() {
-	wp_enqueue_style( 'acstarter-style', get_template_directory_uri()."/style.min.css",array(),'12340' );
+	wp_enqueue_style( 'acstarter-style', get_template_directory_uri()."/style.min.css",array(),'5555' );
 
 	wp_deregister_script('jquery');
 		wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', false, '1.10.2', true);
